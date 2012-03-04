5322f0ed70db43917a1f75c0a0d2997704960aa2
Michael Xavier <michael@michaelxavier.net>
2012-03-04 09:47:42 -0800

    Add refiller process

---
+++ Bucketeer/Refiller.hs
@@ -0,0 +1,23 @@
+module Bucketeer.Refiller (runRefiller) where
+
+import Bucketeer.Types
+import Bucketeer.Persistence (restore)
+
+import Control.Concurrent (forkIO,
+                           ThreadId)
+import Control.Concurrent.Thread.Delay (delay)
+import Control.Monad (forever)
+import Database.Redis (runRedis,
+                       connect,
+                       ConnectInfo)
+
+runRefiller :: ConnectInfo
+               -> Bucket
+               -> IO ThreadId
+runRefiller ci Bucket { consumer = cns,
+                        feature = feat,
+                        capacity = cap,
+                        restoreRate = rate} = forkIO $ loop =<< connect ci
+  where loop conn      = forever $ doRestore conn >> doDelay
+        doRestore conn = runRedis conn $ restore cns feat cap
+        doDelay        = delay $ rate * 1000 
