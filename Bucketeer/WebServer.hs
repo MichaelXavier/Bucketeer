45c41e48711d6341456023b40b2bcdc9a962c73c
Michael Xavier <michael@michaelxavier.net>
2012-03-12 13:33:21 -0700

    Need to switch to IORef in foundation

--- Bucketeer/WebServer.hs
+++ Bucketeer/WebServer.hs
@@ -11,11 +11,17 @@ import Bucketeer.Persistence (remaining,
                               refill,
                               TickResult(..))
 import Bucketeer.Manager (BucketManager,
+                          revokeFeature,
+                          revokeConsumer,
                           startBucketManager,
                           restoreBuckets)
 import Bucketeer.Types
 import Bucketeer.WebServer.Util
 
+import Control.Applicative ((<$>))
+import Control.Concurrent (forkIO,
+                           killThread)
+import Control.Monad.Reader (local)
 import Data.ByteString (ByteString(..))
 import Data.Text (Text(..))
 import Database.Redis (Connection,
@@ -27,7 +33,9 @@ import System.Exit (exitFailure)
 import System.IO (hPutStrLn,
                   stderr)
 import Yesod
+import Yesod.Handler
 
+--TODO: store an IORef here instead
 data BucketeerWeb = BucketeerWeb { connection    :: Connection,
                                    bucketManager :: BucketManager }
 
@@ -37,6 +45,7 @@ instance Yesod BucketeerWeb where
 --TODO: deletion of consumers, buckets?
 
 mkYesod "BucketeerWeb" [parseRoutes|
+  /consumers/#Consumer                         ConsumerR     DELETE
   /consumers/#Consumer/buckets/#Feature        BucketR       GET POST DELETE
   /consumers/#Consumer/buckets/#Feature/tick   BucketTickR   POST
   /consumers/#Consumer/buckets/#Feature/refill BucketRefillR POST
@@ -58,7 +67,10 @@ postBucketR cns feat = undefined
 deleteBucketR :: Consumer
                  -> Feature
                  -> Handler ()
-deleteBucketR cns feat = undefined
+deleteBucketR cns feat = do (newBM, maybeTid) <- revokeFeature cns feat =<< getBM
+                            forkIO . killThread <$> maybeTid
+                            --TODO: set newBM
+                            return ()
 
 --TODO: handle throttle with statuscode
 postBucketTickR :: Consumer
@@ -80,6 +92,15 @@ postBucketDrainR :: Consumer
 postBucketDrainR cns feat = doDrain =<< getConn
   where doDrain conn = liftIO $ runRedis conn $ drain cns feat
 
+
+deleteConsumerR  :: Consumer
+                    -> Feature
+                    -> Handler ()
+deleteConsumerR cns = do (newBM, tids) <- revokeConsumer cns =<< getBM
+                         mapM_ forkIO . killThread tids
+                         --TODO: set newBM
+                         return ()
+
 main :: IO ()
 main = do conn <- connect defaultConnectInfo
           buckets <- either (exit) (return . id) =<< (runRedis conn $ restoreBuckets)
@@ -89,3 +110,4 @@ main = do conn <- connect defaultConnectInfo
 
 ---- Helpers
 getConn = return . connection =<< getYesod
+getBM = return . bucketManager =<< getYesod
