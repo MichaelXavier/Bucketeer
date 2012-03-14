65395acd82bd9ed850e7f668a4f20dff63eea450
Michael Xavier <michael@michaelxavier.net>
2012-03-13 21:48:38 -0700

    Add empty test suite for all HTTP actions

--- Bucketeer/Testing/WebServer.hs
+++ Bucketeer/Testing/WebServer.hs
@@ -16,6 +16,7 @@ import Network.Wai (Application,
 import Network.Wai.Test
 import Test.Hspec.Monadic (Specs,
                            describe,
+                           pending,
                            descriptions,
                            it)
 import Test.Hspec.HUnit
@@ -25,6 +26,117 @@ specs :: Application
          -> Specs
 specs app = do
   let webApp = flip runSession $ app
-  describe "request to a bogus endpoint" $ do
+  describe "GET request to a bogus endpoint" $ do
     it "returns a 404" $ webApp $
-      assertStatus 403 =<< request defaultRequest
+      assertStatus 404 =<< request defaultRequest { rawPathInfo = "/sammiches" }
+
+  describe "DELETE request to consumer" $ do
+    it "non-existant consumer: returns a 404" $ webApp $
+      assertStatus 404 =<< request defaultRequest { rawPathInfo = "/consumers/bogus" }
+    it "returns a 204" $ 
+      pending "implementation"
+
+
+  describe "GET to bucket" $ do
+    it "non-existant bucket: returns a 404" $ 
+      pending "implementation"
+    it "non-existant bucket: returns an error message" $ 
+      pending "implementation"
+
+    it "existing bucket: returns a 200" $ 
+      pending "implementation"
+    it "existing bucket: renders the bucket" $ 
+      pending "implementation"
+
+  describe "POST to bucket" $ do
+    it "non-existant consumer: returns a 404" $ 
+      pending "implementation"
+    it "non-existant consumer: returns an error message" $ 
+      pending "implementation"
+
+    it "existing consumer, all params: returns a 201" $ 
+      pending "implementation"
+    it "existing bucket, all params: renders the bucket" $ 
+      pending "implementation"
+
+    it "existing consumer, missing capacity: returns a 400" $ 
+      pending "implementation"
+    it "existing consumer, missing capacity: returns an error" $ 
+      pending "implementation"
+
+    it "existing consumer, missing restore_rate: returns a 400" $ 
+      pending "implementation"
+    it "existing consumer, missing restore_rate: returns an error" $ 
+      pending "implementation"
+
+    it "existing consumer, missing both: returns a 400" $ 
+      pending "implementation"
+    it "existing consumer, missing both: returns an error" $ 
+      pending "implementation"
+
+
+  describe "DELETE to bucket" $ do
+    it "non-existant consumer: returns a 404" $ 
+      pending "implementation"
+    it "non-existant consumer: returns an error message" $ 
+      pending "implementation"
+
+    it "non-existant bucket: returns a 404" $ 
+      pending "implementation"
+    it "non-existant bucket: returns an error message" $ 
+      pending "implementation"
+
+    it "existing bucket: returns 204" $ 
+      pending "implementation"
+    it "existing bucket: removes the bucket from the manager" $ 
+      pending "implementation"
+    it "existing bucket: revokes the bucket in redis" $ 
+      pending "implementation"
+
+  describe "POST to bucket tick" $ do
+    it "non-existant consumer: returns a 404" $ 
+      pending "implementation"
+    it "non-existant consumer: returns an error message" $ 
+      pending "implementation"
+
+    it "non-existant bucket: returns a 404" $ 
+      pending "implementation"
+    it "non-existant bucket: returns an error message" $ 
+      pending "implementation"
+
+    it "existing bucket: returns 204" $ 
+      pending "implementation"
+    it "existing bucket: ticks the bucket" $ 
+      pending "implementation"
+
+  describe "POST to bucket refill" $ do
+    it "non-existant consumer: returns a 404" $ 
+      pending "implementation"
+    it "non-existant consumer: returns an error message" $ 
+      pending "implementation"
+
+    it "non-existant bucket: returns a 404" $ 
+      pending "implementation"
+    it "non-existant bucket: returns an error message" $ 
+      pending "implementation"
+
+    it "existing bucket: returns 204" $ 
+      pending "implementation"
+    it "existing bucket: refills the bucket" $ 
+      pending "implementation"
+
+  describe "POST to bucket drain" $ do
+    it "non-existant consumer: returns a 404" $ 
+      pending "implementation"
+    it "non-existant consumer: returns an error message" $ 
+      pending "implementation"
+
+    it "non-existant bucket: returns a 404" $ 
+      pending "implementation"
+    it "non-existant bucket: returns an error message" $ 
+      pending "implementation"
+
+    it "existing bucket: returns 204" $ 
+      pending "implementation"
+    it "existing bucket: drains the bucket" $ 
+      pending "implementation"
